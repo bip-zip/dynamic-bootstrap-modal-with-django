cb7e966deba8da94ef8f3e2fb39983816c8c2cb1
bip-zip <bipindhakal44@gmail.com>
2024-01-26 14:28:55 +0545

    add models

--- modalapp/admin.py
+++ modalapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import ParkingLot, Vehicle
 
-# Register your models here.
+class VehicleInline(admin.TabularInline):
+    model = Vehicle
+    extra = 0
+
+class ParkingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'vehicle_capacity' )
+    inlines = [VehicleInline]
+
+admin.site.register(ParkingLot, ParkingAdmin)
--- modalapp/models.py
+++ modalapp/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+
+class ParkingLot(models.Model):
+    name = models.CharField(max_length = 50, null=False, unique=True)
+    vehicle_capacity = models.IntegerField(default=1)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    @property
+    def remaining_space(self):
+        pass
+    
+class Vehicle(models.Model):
+    VEHICLE_TYPE=(("Bike", "bike"),
+                ("Car", "car"),
+                ("Scooter", "scooter"),)
+
+    parkinglot= models.ForeignKey(ParkingLot, on_delete =models.CASCADE)
+    vehicle_type= models.CharField(max_length = 12, choices = VEHICLE_TYPE, default ='bike')
+    vehicle_number = models.CharField(max_length = 50, null=False, blank=False)
+    color= models.CharField(max_length = 20, null=False)
+    brand = models.CharField(max_length = 30, null= False)
+    entry_time = models.DateTimeField(auto_now = True)
+
+    def __str__(self) -> str:
+        return self.vehicle_number
